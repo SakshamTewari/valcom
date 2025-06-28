5a75214d583f48b13cfc5cbb3ea7d2cd585fa37f
Saksham Tewari <sakshamtewari3@gmail.com>
2025-06-28 23:18:15 +0530

    add: User Disconnect Socket handler

---
+++ server/src/socket/handlers/user.handler.ts
@@ -0,0 +1,3 @@
+export const disconnectEventHandler = (socketId: string) => {
+  console.log(`User disconnected of the id: ${socketId}`);
+};
--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,11 +1,12 @@
 import { Server, Socket } from 'socket.io';
+import { disconnectEventHandler } from './handlers/user.handler';
 
 export const setupSocket = (io: Server) => {
   io.on('connection', (socket: Socket) => {
     console.log(`User connected : ${socket.id}`);
 
     socket.on('disconnect', () => {
-      console.log(`User disconnected: ${socket.id}`);
+      disconnectEventHandler(socket.id);
     });
   });
 };
