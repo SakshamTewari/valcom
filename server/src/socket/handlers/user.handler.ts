cc1e142c189b3babbf03e0a3f3f8278c54a5ba43
Saksham Tewari <sakshamtewari3@gmail.com>
2025-07-04 21:14:45 +0530

    add: loginEventHandler

--- server/src/socket/handlers/user.handler.ts
+++ server/src/socket/handlers/user.handler.ts
@@ -1,3 +1,31 @@
+import { Socket } from 'socket.io';
+
+interface User {
+  username: string;
+  coords: {
+    lat: number;
+    lng: number;
+  };
+}
+
+const onlineUsers: Record<string, User> = {};
+
 export const disconnectEventHandler = (socketId: string) => {
   console.log(`User disconnected of the id: ${socketId}`);
+  removeOnlineUser(socketId); // clean up user
+};
+
+const removeOnlineUser = (socketId: string) => {
+  if (onlineUsers[socketId]) delete onlineUsers[socketId];
+
+  console.log(onlineUsers);
+};
+
+export const loginEventHandler = (socket: Socket, data: User) => {
+  onlineUsers[socket.id] = {
+    username: data.username,
+    coords: data.coords,
+  };
+  console.log(`User logged in: ${data.username}`, onlineUsers);
+  console.log('Online users: ', onlineUsers);
 };
--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,9 +1,16 @@
 import { Server, Socket } from 'socket.io';
-import { disconnectEventHandler } from './handlers/user.handler';
+import {
+  disconnectEventHandler,
+  loginEventHandler,
+} from './handlers/user.handler';
 
 export const setupSocket = (io: Server) => {
   io.on('connection', (socket: Socket) => {
     console.log(`User connected : ${socket.id}`);
+    console.log('📨 Handshake headers:', socket.handshake.headers);
+    console.log('🧠 User-Agent:', socket.handshake.headers['user-agent']);
+
+    socket.on('user-login', (data) => loginEventHandler(socket, data));
 
     socket.on('disconnect', () => {
       disconnectEventHandler(socket.id);
